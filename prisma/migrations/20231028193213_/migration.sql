267cd988add0b1ddbf4c1114bcb96258ae30b64d
André Oliveira <andreoliveira@luizalabs.com>
2023-10-28 17:42:32 -0300

    creating send message

---
+++ prisma/migrations/20231028193213_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "reservations" ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'pending';
--- src/controllers/reservation.controller.ts
+++ src/controllers/reservation.controller.ts
@@ -1,15 +1,21 @@
 import {
   Body,
-  ConflictException,
   Controller,
   Get,
   HttpCode,
+  HttpException,
+  HttpStatus,
+  Param,
   Post,
   UsePipes,
 } from '@nestjs/common'
 import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { z } from 'zod'
+import * as twilio from 'twilio'
+
+const accountSid = 'AC116c33c4b022251d4d1f7aaa56bcd1ce'
+const authToken = '3306e1d881634d1bcf703776db46b06a'
 
 const createReservationBodySchema = z.object({
   name: z.string(),
@@ -33,20 +39,46 @@ export class CreateReservationController {
   async postReservations(@Body() body: CreateReservationBodySchema) {
     const { name, phone } = body
 
-    const userWithSameEmail = await this.prisma.reservation.findUnique({
-      where: {
-        phone,
-      },
-    })
-
-    if (userWithSameEmail)
-      throw new ConflictException('User with same email already exists')
-
     await this.prisma.reservation.create({
       data: {
         name,
         phone,
       },
     })
+  }
+
+  @Post('/message/:userID')
+  @HttpCode(200)
+  async postMessage(@Param('userID') userID: string, @Body() body: any) {
+    const client = twilio(accountSid, authToken)
+
+    const account = await this.prisma.reservation.findUnique({
+      where: {
+        id: userID,
+      },
+    })
+
+    if (account) {
+      await client.messages
+        .create({
+          body: body.message,
+          from: 'whatsapp:+14155238886',
+          to: `whatsapp:+55${account.phone}`,
+        })
+        .then(async () => {
+          await this.prisma.reservation.update({
+            where: { id: userID },
+            data: { status: 'confirmed' },
+          })
+        })
+        .catch((error) => {
+          throw new HttpException(
+            'Erro ao enviar a mensagem: ' + error.message,
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          )
+        })
+    } else {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND)
+    }
   }
 }
