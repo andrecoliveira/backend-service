3a6c8724faa0509cbc17335471d11b208da4937c
André Oliveira <andreoliveira@luizalabs.com>
2023-12-19 11:26:34 -0300

    feat: create new user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateReservationController } from './controllers/reservation.controller'
 import { CreateRestaurantController } from './controllers/restaurant.controller'
+import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
 
 @Module({
@@ -12,7 +13,11 @@ import { envSchema } from './env'
       isGlobal: true,
     }),
   ],
-  controllers: [CreateReservationController, CreateRestaurantController],
+  controllers: [
+    CreateReservationController,
+    CreateRestaurantController,
+    CreateAccountController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
