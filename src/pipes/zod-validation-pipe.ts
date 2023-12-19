181e0a0b19ce1db1a9dba81b1e6c3969393d5a85
André Oliveira <andreoliveira@luizalabs.com>
2023-12-19 11:25:49 -0300

    refactor: improvement validation

--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -1,5 +1,6 @@
 import { PipeTransform, BadRequestException } from '@nestjs/common'
-import { ZodSchema } from 'zod'
+import { ZodError, ZodSchema } from 'zod'
+import { fromZodError } from 'zod-validation-error'
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
@@ -8,6 +9,14 @@ export class ZodValidationPipe implements PipeTransform {
     try {
       this.schema.parse(value)
     } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          message: 'Validation failed',
+          statusCode: 400,
+          errors: fromZodError(error),
+        })
+      }
+
       throw new BadRequestException('Validation failed')
     }
 
