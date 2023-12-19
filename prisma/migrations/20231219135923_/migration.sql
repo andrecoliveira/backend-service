531d2f282f24b4738dae43268a5330ba022db406
André Oliveira <andreoliveira@luizalabs.com>
2023-12-19 11:26:54 -0300

    feat: create user schema

--- prisma/migrations/20231219135923_/migration.sql
+++ prisma/migrations/20231219135923_/migration.sql
@@ -1,4 +1,17 @@
 -- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "reservations" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -30,3 +43,6 @@ CREATE TABLE "restaurants" (
 
     CONSTRAINT "restaurants_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
