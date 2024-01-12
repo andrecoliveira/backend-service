46b6f21b0a7cc2312b485117fc1e8fe0961fcc1e
André Oliveira <andreoliveira@luizalabs.com>
2024-01-12 16:16:20 -0300

    feat: incluindo msg automática

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -14,26 +14,22 @@ export class WebhookController {
   @HttpCode(200)
   async sendMessage(@Body() body: any, @Response() res: any) {
     console.log('BODY: ', body)
-    // const {
-    //   phone,
-    //   text: { message },
-    // } = body
 
-    // if (phone === '558597496471' && message === 'Olá') {
-    //   try {
-    //     const response = await instance.post(
-    //       'https://api.z-api.io/instances/3C9395131E4380A8AC883A6238F22DFA/token/254F6FD8A40E02F2C93DE806/send-text',
-    //       {
-    //         phone,
-    //         message: 'Olá, tudo bem minha Felipe?',
-    //       },
-    //     )
+    if (body.phone === '558597496471' && body.text.message === 'webhook') {
+      try {
+        const response = await instance.post(
+          'https://api.z-api.io/instances/3C9395131E4380A8AC883A6238F22DFA/token/254F6FD8A40E02F2C93DE806/send-text',
+          {
+            phone: body.phone,
+            message: 'É isso aí Felipe, agora o negócio está automático.',
+          },
+        )
 
-    //     // console.log('Mensagem enviada com sucesso:', response.data)
-    //   } catch (error) {
-    //     // console.error('Erro ao enviar mensagem:', error)
-    //   }
-    // }
+        console.log('Mensagem enviada com sucesso:', response.data)
+      } catch (error) {
+        console.error('Erro ao enviar mensagem:', error)
+      }
+    }
 
     res.send({ message: 'ok' })
   }
