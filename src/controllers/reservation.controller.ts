e321f54a6a91e94c31fbde677e5c316b2216acae
André Oliveira <andreoliveira@luizalabs.com>
2024-01-12 15:29:37 -0300

    refactor: testando tipos de envios

--- src/controllers/reservation.controller.ts
+++ src/controllers/reservation.controller.ts
@@ -3,8 +3,6 @@ import { AuthGuard } from '@nestjs/passport'
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL:
-    'https://api.z-api.io/instances/3C9395131E4380A8AC883A6238F22DFA/token/254F6FD8A40E02F2C93DE806/send-text',
   timeout: 1000,
   headers: { 'client-token': 'Ff854860376ba486db3656fb87afcc396S' },
 })
