6fee604dd951702a23344622f09f14211b86481c
André Oliveira <andreoliveira@luizalabs.com>
2024-01-12 13:37:48 -0300

    feat: added request to whatsapp api

--- src/controllers/reservation.controller.ts
+++ src/controllers/reservation.controller.ts
@@ -11,6 +11,7 @@ import { AuthGuard } from '@nestjs/passport'
 import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
 import { PrismaService } from '@/prisma/prisma.service'
 import { z } from 'zod'
+import axios from 'axios'
 
 const createReservationBodySchema = z.object({
   name: z.string(),
@@ -34,6 +35,26 @@ export class CreateReservationController {
   @UsePipes(new ZodValidationPipe(createReservationBodySchema))
   async postReservations(@Body() body: CreateReservationBodySchema) {
     const { name, phone } = body
+
+    try {
+      const response = await axios.post(
+        'https://api.z-api.io/instances/3C9395131E4380A8AC883A6238F22DFA/token/254F6FD8A40E02F2C93DE806/send-text',
+        {
+          phone,
+          message: name,
+        },
+        {
+          headers: {
+            Authorization: 'Bearer 254F6FD8A40E02F2C93DE806',
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+
+      console.log('Mensagem enviada com sucesso:', response.data)
+    } catch (error) {
+      console.error('Erro ao enviar mensagem:', error)
+    }
 
     await this.prisma.reservation.create({
       data: {
