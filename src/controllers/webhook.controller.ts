54cefc9a2d9f231cf4fd76f5aa40e1ac3eec65e4
André Oliveira <andreoliveira@luizalabs.com>
2024-01-12 14:28:03 -0300

    feat: adicionando resposta automática

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -1,11 +1,38 @@
 import { Body, Controller, HttpCode, Post, Response } from '@nestjs/common'
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL:
+    'https://api.z-api.io/instances/3C9395131E4380A8AC883A6238F22DFA/token/254F6FD8A40E02F2C93DE806/send-text',
+  timeout: 1000,
+  headers: { 'client-token': 'Ff854860376ba486db3656fb87afcc396S' },
+})
 
 @Controller('webhook')
 export class WebhookController {
   @Post()
   @HttpCode(200)
   async sendMessage(@Body() body: any, @Response() res: any) {
-    console.log(body)
+    const {
+      phone,
+      text: { message },
+    } = body
+
+    if (phone === '558588850088' && message === 'oi') {
+      try {
+        const response = await instance.post(
+          'https://api.z-api.io/instances/3C9395131E4380A8AC883A6238F22DFA/token/254F6FD8A40E02F2C93DE806/send-text',
+          {
+            phone,
+            message: 'Olá, tudo bem minha gatinha?',
+          },
+        )
+
+        console.log('Mensagem enviada com sucesso:', response.data)
+      } catch (error) {
+        console.error('Erro ao enviar mensagem:', error)
+      }
+    }
 
     res.send({ message: 'ok' })
   }
