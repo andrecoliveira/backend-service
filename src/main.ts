743067ac92e7cf41f8d62cf1e6ff7a94b9ecae64
André Oliveira <andreoliveira@luizalabs.com>
2024-01-14 15:59:08 -0300

    refactor: updated import cookieParser

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 import { NestExpressApplication } from '@nestjs/platform-express'
-import * as cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser'
 import { Env } from './env'
 
 async function bootstrap() {
